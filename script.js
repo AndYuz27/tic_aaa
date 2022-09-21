c4cd0fb58af8bc8b7741323e8b9c502730ccf63d
AndYuz27 <yuzlov2003@gmail.com>
2022-09-21 17:14:02 +0300

    first commit

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 import f from "./func/11.js"
 const board = document.querySelector(".board");
-const n = 3;
-const chars= ["X", "0"]
-let stepCnt = n**3
-let step = 0
-f.setField(n, board)
+const field = [];
+let n = 3;
+const chars = ["⮾", "⦾"];
+f.setField(n, board, field);
+let stepCnt = n ** 3;
+let step = 0;
 
-const cells = board.querySelectorAll('.board__cell')
 
-cells.forEach(cell =>{
-    cell.addEventListener("click", function(e)
-    {
-        cell.innerHTML = chars[step]
-        step = +!step
-    })
-})
+// TODO: Создать поле ввода числа на основании которого рисуется поле для игры
+
+const cells = board.querySelectorAll(".board__cell");
+
+cells.forEach((cell, i) => {
+    cell.addEventListener("click", function(e) {
+        if (stepCnt) {
+            if (!field[i]) {
+                field[i] = chars[step];
+                cell.innerHTML = chars[step];
+                console.log(f.isWin(field, step, n));
+                step = +!step;
+                stepCnt--;
+            }
+        }
+    });
+});
